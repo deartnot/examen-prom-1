768ba7660ad5131091bd2126d42f6886b66dec8a
deartnot <fabrizio.farnet@hotmail.com>
2024-09-18 23:39:58 -0500

    agregado de personaje  y enemigo
    variables de ellos

--- Program.cs
+++ Program.cs
@@ -33,4 +33,43 @@ namespace examen2._0
             mana = costoMana; 
         }
     }
+
+    class Personaje
+    {
+        public string Nombre;
+        public int Vida;
+        public int Ataque;
+        public List<Item> Items;
+
+        public Personaje(string name, int vida, int ataque)
+        {
+            Nombre = name;
+            Vida = vida;
+            Ataque = ataque;
+            Items = new List<Item>();
+        }
+
+        public bool EstaMuerto()
+        {
+            return Vida <= 0; // si la vida es menor o igual a 0, está muerto
+        }
+
+        public void Atacar(Personaje enemigo)
+        {
+            enemigo.Vida = enemigo.Vida - Ataque;
+            Console.WriteLine(Nombre + " atacó a " + enemigo.Nombre + ", hizo " + Ataque + " de daño. Vida restante de " + enemigo.Nombre + ": " + enemigo.Vida);
+        }
+    }
+
+
+    class Enemigo : Personaje
+    {
+        public string tipo; // si es "melee" o "rango"
+
+        public Enemigo(string name, int vida, int ataque, string tipoDeEnemigo)
+            : base(name, vida, ataque)
+        {
+            tipo = tipoDeEnemigo;
+        }
+    }
 }
